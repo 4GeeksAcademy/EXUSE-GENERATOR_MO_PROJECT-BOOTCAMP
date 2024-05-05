1e43759de7f286295912f686ff9323825cf2d30e
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-05-05 06:57:10 +0000

    actualizado formato

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico"; */
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here  // listado opciones excusas //
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the phone", "the car"];
+  const when = [
+    "before the class",
+    "When I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Función para generar una excusa aleatoria
+  function generateExcuse() {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+    console.log(randomWho);
+
+    const excuse =
+      randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+    document.querySelector("#Excuse").textContent = excuse;
+    console.log(excuse);
+    // console.log(document.getElementById("Excuse"));
+  }
+
+  generateExcuse();
 };
